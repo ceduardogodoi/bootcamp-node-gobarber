d4384c531651629eb996a7ae28ef8dcb6ac945f0
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-11-21 23:41:18 -0300

    Configurando o nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '055d991324f3d9',
+    pass: '9e55000f3c9e3a',
+  },
+  default: {
+    from: 'Equipre GoBarber <noreply@gobarber.com>',
+  },
+};
